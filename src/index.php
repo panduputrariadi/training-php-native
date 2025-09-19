f79e3a94885223b4db7ba48c574f5b000a1f50e0
panduputragit <put.rariadi1144@gmail.com>
2025-09-19 09:47:36 +0800

    fix: restructure file, split code routes, delete unescessary code comment

--- src/index.php
+++ src/index.php
@@ -1,26 +1,13 @@
 <?php
 namespace PanduputragmailCom\PhpNative\Lib;
-use PanduputragmailCom\PhpNative\Controller\Controller;
-use PanduputragmailCom\PhpNative\Controller\DummyDataController;
+
+use PanduputragmailCom\PhpNative\Lib\Routing;
+use PanduputragmailCom\PhpNative\Routes\Api;
 
 require __DIR__.'/../vendor/autoload.php';
 
 $router = new Routing();
-$router->add('GET', '/', function() {
-    return 'Hello world';
-});
 
-// $router->add('GET', '/connect-db', function() {
-//     $controller = new Controller();
-//     $connection = $controller->ConnectDB();
-    
-//     // Jika ingin menampilkan pesan sukses
-//     return $connection;
-// });
-$router->add('GET', '/connect-db', [Controller::class, 'ConnectDB']);
-$router->add('GET', '/get-dummy-data', [DummyDataController::class, 'GetDataDummy']);
-$router->add('POST', '/store-dummy-data', [DummyDataController::class, 'store']);
+$router = Api::registerRoutes($router);
 
 $router->run();
-
-// echo "Hello world";
